cd3b721e5919580f03ff0662a82a0f6d22c4eb88
Eric Youngberg <eyoungberg@mapc.org>
2019-04-12 15:09:23 -0400

    Finish process.py script

--- process.py
+++ process.py
@@ -2,19 +2,45 @@
 
 from sys import exit
 from glob import glob
-from os import path, system
 from datetime import datetime
 from random import getrandbits
 from tempfile import gettempdir
+from os import listdir, mkdir, makedirs, path, system, unlink
 from shutil import copy, copytree, make_archive, rmtree
+
+
+def _clear_directory(dir):
+    for fd in listdir(dir):
+        if fd == '.gitkeep':
+            continue
+
+        file_path = path.join(dir, fd)
+
+        try:
+            if path.isfile(file_path):
+                unlink(file_path)
+            elif path.isdir(file_path):
+                _clear_directory(file_path)
+        except Exception as e:
+            print(e)
+
+def cwd():
+    return path.dirname(path.realpath(__file__))
 
 
 # Define run steps for each processing component
 
+def clear_directories():
+    _clear_directory(path.join(cwd(), 'volumes', 'cleaner', 'csv'))
+    _clear_directory(path.join(cwd(), 'volumes', 'cleaner', 'output'))
+    _clear_directory(path.join(cwd(), 'volumes', 'geolocator', 'data'))
+    _clear_directory(path.join(cwd(), 'volumes', 'geolocator', 'output'))
+
+
 def run_mapper():
     print("[--] Running Mapper")
 
-    if not system('docker-compose up mapper'):
+    if system('docker-compose up mapper') != 0:
         print('[!!] Mapper failed')
         exit(1)
 
@@ -22,20 +48,18 @@ def run_mapper():
 def run_cleaner():
     print("[--] Running Cleaner")
 
-    if not system('docker-compose up cleaner'):
+    if system('docker-compose up cleaner') != 0:
         print('[!!] Cleaner failed')
         exit(1)
 
     print("[--] Preparing cleaned data for geolocation")
 
-    cwd = path.dirname(path.realpath(__file__))
-
-    src_dir = path.join(cwd, 'volumes', 'cleaner', 'output')
+    src_dir = path.join(cwd(), 'volumes', 'cleaner', 'output')
     src = glob(path.join(src_dir, '*_listings_unique.csv'))
     if len(src) > 0:
         src = src[0]
 
-    dest = path.join(cwd, 'volumes', 'geolocator', 'data')
+    dest = path.join(cwd(), 'volumes', 'geolocator', 'data')
 
     try:
         copy(src, dest)
@@ -48,47 +72,46 @@ def run_cleaner():
 def run_geolocator():
     print("[--] Running Geolocator")
 
-    if not system('docker-compose up geolocator'):
+    if system('docker-compose up geolocator') != 0:
         print('[!!] Geolocator failed')
         exit(1)
 
     print("[--] Bundling output")
 
-    workdir = path.join(gettempdir(), '%0x-rla-out' % getrandbits(40))
-    workdir_cleaner = path.join(workdir, 'cleaner')
-    workdir_geolocator = path.join(workdir, 'geolocator')
-
-    try:
-        dir_perms = 0o700
-        mkdir(workdir, dir_perms)
-        mkdir(workdir_cleaner, dir_perms)
-        mkdir(workdir_geolocator, dir_perms)
-    except OSError:
-        print("[!!] Could not create %s. You will have to bundle your own output." % workdir)
-        exit(1)
-
-    cwd = path.dirname(path.realpath(__file__))
-    copytree(path.join(cwd, 'volumes', 'cleaner', 'output'), workdir_cleaner) 
-    copytree(path.join(cwd, 'volumes', 'geolocator', 'output'), workdir_geolocator) 
-
     env = {}
-    with open(path.join(cwd, ".env.mapper")) as fd:
+    with open(path.join(cwd(), ".env.mapper")) as fd:
         for line in fd:
             tokens = line.split('=')
             if len(tokens) > 1:
                 name, var = line.split('=')
                 env[name.strip()] = str(var).strip()
 
-    make_archive(path.join(cwd, 'rental-listings_%02d-%d' % (int(env['MAPPER_QUARTER']), int(env['MAPPER_YEAR']))), 'zip', workdir)
+    archive_name = 'rental-listings_Q%s-%s' % (env['MAPPER_QUARTER'], env['MAPPER_YEAR'])
+    tempdir = path.join(gettempdir(), 'rla-out-%0x' % getrandbits(40))
+    workdir = path.join(tempdir, archive_name)
+    workdir_cleaner = path.join(workdir, 'cleaner')
+    workdir_geolocator = path.join(workdir, 'geolocator')
 
     try:
-        rmtree(workdir)
+        makedirs(workdir, 0o700, exist_ok=True)
+    except OSError:
+        print("[!!] Could not create %s. You will have to bundle your own output." % workdir)
+        exit(1)
+
+    copytree(path.join(cwd(), 'volumes', 'cleaner', 'output'), workdir_cleaner) 
+    copytree(path.join(cwd(), 'volumes', 'geolocator', 'output'), workdir_geolocator) 
+
+    make_archive(path.join(cwd(), archive_name), 'zip', tempdir)
+
+    try:
+        rmtree(tempdir)
     except OSError:
         print("[!!] Could not delete tempdir %s. Restarting machine will clear all files in temp directory." % workdir)
 
 
 # Run processor
 
+clear_directories()
 run_mapper()
 run_cleaner()
 run_geolocator()
