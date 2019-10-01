f3e75109be4303405c7a543fb177259d2ba7e6ff
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-10-01 13:42:07 -0400

    Add files to linked directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/opt/rental-listing-processor"
 append :linked_files, ".env.mapper"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "files"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
